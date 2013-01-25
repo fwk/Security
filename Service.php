33e2766450ed55a204a377dd40e5e248ef0fb816
neiluJ <julien@nitronet.org>
2013-01-25 01:54:23 +0100

    refactored things...

--- Service.php
+++ Service.php
@@ -2,6 +2,7 @@
 namespace Fwk\Security;
 
 use Fwk\Security\Authentication\Manager as AuthManager;
+use Fwk\Security\Acl\Manager as AclManager;
 use Fwk\Security\User\Provider as UserProvider;
 
 class SecurityService
@@ -28,17 +29,27 @@ class SecurityService
     protected $authenticationManager;
     
     /**
+     * Acl Manager
+     * 
+     * @var AclManager
+     */
+    protected $aclManager;
+    
+    /**
      * Constructor
      * 
-     * @param AuthManager  $manager  The Authentification Manager
-     * @param UserProvider $provider The User Provider
+     * @param AuthManager  $manager    The Authentification Manager
+     * @param UserProvider $provider   The User Provider
+     * @param AclManager   $aclManager The Acl Manager (if any)
      * 
      * @return void
      */
-    public function __construct(AuthManager $manager, UserProvider $provider)
+    public function __construct(AuthManager $manager, UserProvider $provider,
+            AclManager $aclManager = null)
     {
         $this->authenticationManager    = $manager;
         $this->userProvider             = $provider;
+        $this->aclManager               = $aclManager;
     }
     
     /**
@@ -98,6 +109,30 @@ class SecurityService
     public function setAuthenticationManager(AuthManager $authenticationManager)
     {
         $this->authenticationManager = $authenticationManager;
+        
+        return $this;
+    }
+    
+    /**
+     * Returns the Acl Manager
+     * 
+     * @return AclManager 
+     */
+    public function getAclManager() 
+    {
+        return $this->aclManager;
+    }
+
+    /**
+     * Defines the Authentication Manager
+     * 
+     * @param AclManager $aclManager Acl Manager
+     * 
+     * @return SecurityService
+     */
+    public function setAclManager(AclManager $aclManager)
+    {
+        $this->aclManager = $aclManager;
         
         return $this;
     }
